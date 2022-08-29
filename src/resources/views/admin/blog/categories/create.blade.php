cb43880be5e14b601c5abc3767b6ab2c1dc02200
Maxim Klassen <himmelmaler@gmail.com>
2022-08-29 12:29:11 +0200

    tinymce included

--- src/resources/views/admin/blog/categories/create.blade.php
+++ src/resources/views/admin/blog/categories/create.blade.php
@@ -146,6 +146,12 @@
         inputFileImg(previewInput)
     }
     autoSlug('.autoslug')
+    tinymce.init({
+      selector: 'textarea#description',
+      plugins: ' advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinymcespellchecker',
+      toolbar: 'undo redo styleselect bold italic link image alignleft aligncenter alignright permanentpen table tableofcontents',
+      toolbar_mode: 'floating',
+    });
     </script>
 
 @endsection
--- src/resources/views/admin/blog/posts/edit.blade.php
+++ src/resources/views/admin/blog/posts/edit.blade.php
@@ -193,6 +193,20 @@
     autoSlug('.autoslug')
 
     tagFormInit()
+
+    tinymce.init({
+      selector: 'textarea#description',
+      plugins: ' advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinymcespellchecker',
+      toolbar: 'undo redo styleselect bold italic link image alignleft aligncenter alignright permanentpen table tableofcontents',
+      toolbar_mode: 'floating',
+    });
+
+    tinymce.init({
+      selector: 'textarea#text',
+      plugins: ' advcode casechange export formatpainter image editimage linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tableofcontents tinymcespellchecker',
+      toolbar: 'undo redo styleselect bold italic link image alignleft aligncenter alignright permanentpen table tableofcontents',
+      toolbar_mode: 'floating',
+    });
     </script>
 
 @endsection
